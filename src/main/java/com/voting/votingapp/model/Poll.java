536b33b87c26dcc098da99ef1c5a43e5f9bb5dc1
payalsingh1110 <payalsingh.111020@gmail.com>
2025-08-23 18:14:39 +0530

    fix:Refactoring Schema

--- src/main/java/com/voting/votingapp/model/Poll.java
+++ src/main/java/com/voting/votingapp/model/Poll.java
@@ -18,8 +18,8 @@ public class Poll {
     private String question;
 
     @ElementCollection
-    private List<String> options = new ArrayList<>();
+    private List<OptionVote> options = new ArrayList<>();
 
-    @ElementCollection
-    private List<Long> votes = new ArrayList<>();
+//    @ElementCollection
+//    private List<Long> votes = new ArrayList<>();
 }
